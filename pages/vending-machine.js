19315d6e6caa726db212e91adef0e9d3f78d9eda
Konstantin <konstantinstepanyan99@gmail.com>
2022-04-01 01:54:49 +0300

    done basic metamask logic

--- pages/vending-machine.js
+++ pages/vending-machine.js
@@ -1,8 +1,29 @@
 import Head from 'next/head';
+import { useState } from 'react';
+import Web3 from 'web3';
 import 'bulma/css/bulma.css';
 import styles from '../styles/VendingMachine.module.css';
 
-export default function VendingMachine() {
+const VendingMachine = () => {
+    const [error, setError] = useState('');
+    let web3;
+    //window.ethereum
+    const connectWalletHandler = async () => {
+
+        if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+            try {
+                await window.ethereum.request({ method: "eth_requestAccounts" })
+                web3 = new Web3(window.ethereum)
+            }
+            catch (err) {
+                setError(err.message)
+            }
+        }
+
+        else {
+            console.log('Please install Metamask');
+        }
+    }
     return (
         <div className={styles.main}>
             <Head>
@@ -12,14 +33,26 @@ export default function VendingMachine() {
             </Head>
             <nav className="navbar mt-4 mb-4">
                 <div className="container">
-                    <div className="navbar-brand">
+                    <div className="navbar-brand" style={{ margin: 0 }}>
                         <h1>Vending Machine</h1>
                     </div>
                     <div className="navbar-end">
-                        <button className="button is-primary">Connect Wallet</button>
+                        <button className="button is-primary" onClick={connectWalletHandler}>Connect Wallet</button>
                     </div>
                 </div>
             </nav>
+            <section>
+                <div className="container">
+                    <p>placeholder text</p>
+                </div>
+            </section>
+            <section>
+                <div className="container has-text-danger">
+                    <p>{error}</p>
+                </div>
+            </section>
         </div>
     )
 }
+
+export default VendingMachine;
